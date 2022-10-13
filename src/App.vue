f360a17e87d6184d7d7bf141c4ddeddf5b94ea8f
Yang Di <young@deelol.com>
2022-10-13 18:55:32 +0800

    dark mode

--- src/App.vue
+++ src/App.vue
@@ -12,11 +12,11 @@ const toggleDark = useToggle(isDark)
 </script>
 
 <template>
-  <main px-16 py-40 text-center>
-    <button @click="addFuncOne">增加{{ countStore.count }}</button>
+  <main px-16 py-40 text-center text-14 color-gray-400>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
     <router-view />
+    <button @click="addFuncOne">增加{{ countStore.count }}</button>
     <p mt-20 text-14 color-gray-400>Flex骰子</p>
   </main>
 </template>
